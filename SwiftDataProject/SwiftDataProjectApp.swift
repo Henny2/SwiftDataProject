b68604d883786f44c813fc1da045ad8f656ba54d
Henny2 <henrieke.baunack@web.de>
2023-12-17 16:41:03 -0800

    Initial Commit

---
+++ SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Henrieke Baunack on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
